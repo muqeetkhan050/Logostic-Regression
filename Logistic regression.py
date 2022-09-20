1a599592ce80244b369ca2830f0a3043c5168d1e
muqeet khan <80127354+muqeetkhan050@users.noreply.github.com>
2022-09-20 23:58:02 +0500

    Update Logistic regression.py

--- Logistic regression.py
+++ Logistic regression.py
@@ -47,8 +47,8 @@ cm=confusion_matrix(y_test,y_pred)
 #visualising the training set result
 from matplotlib.colors import ListedColormap
 X_set,y_set=X_train,y_train
-X1,X2=np.meshgrid(np.arange(start=X_set[:,0].min()-1,stop=X_set(:,0).max()+1,step=0.01),
-      np.meshgrid(np.arange(start=X_set[:,1].min()-1,stop=X_set(:,1).max()+1,step=0.01))
+X1,X2=np.meshgrid(np.arange(start=X_set[:,0].min()-1,stop=X_set[:,0].max()+1,step=0.01),
+                  np.arange(start=X_set[:,1].min()-1,stop=X_set[:,1].max()+1,step=0.01))
       
 plt.contour(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
 alpha=0.75,cmap=ListedColormap(('red','green')))
