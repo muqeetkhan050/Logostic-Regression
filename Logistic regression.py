11e8aad53a72b601990a796edc44aa860419c60c
muqeet khan <80127354+muqeetkhan050@users.noreply.github.com>
2022-09-21 02:40:49 +0500

    Update Logistic regression.py

--- Logistic regression.py
+++ Logistic regression.py
@@ -40,15 +40,43 @@ classifier.fit(X_train,y_train)
 y_pred=classifier.predict(X_test)
 
 #Making confusion Matrix
+
 from matplotlib.colors import ListedColormap
 X_set,y_set=X_train,y_train
 X1,X2=np.meshgrid(np.arange(start=X_set[:,0].min()-1,stop=X_set[:,0].max()+1,step=0.01),
                   np.arange(start=X_set[:,1].min()-1,stop=X_set[:,1].max()+1,step=0.01))
       
-plt.contour(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
 alpha=0.75,cmap=ListedColormap(('red','green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
 
-plt.title("logistic regression(training test)"
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j,0],X_set[y_set==j,i],
+                c=ListedColormap(('red','green'))(i),label=j)
+
+plt.title("logistic regression(training test)")
+plt.xlabel("Age")
+plt.ylabel("Salary")
+plt.legend()
+plt.show()
+
+#visualizing test set
+from matplotlib.colors import ListedColormap
+X_set,y_set=X_test,y_test
+X1,X2=np.meshgrid(np.arange(start=X_set[:,0].min()-1,stop=X_set[:,0].max()+1,step=0.01),
+                  np.arange(start=X_set[:,1].min()-1,stop=X_set[:,1].max()+1,step=0.01))
+      
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+alpha=0.75,cmap=ListedColormap(('red','green')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j,0],X_set[y_set==j,i],
+                c=ListedColormap(('red','green'))(i),label = j)
+
+plt.title("logistic regression( test)")
 plt.xlabel("Age")
 plt.ylabel("Salary")
 plt.legend()
